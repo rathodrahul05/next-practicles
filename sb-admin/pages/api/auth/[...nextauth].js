27577ef4f6e3924ed92393fa441c4e1ae79902da
rahul.rathod <rahul.rathod@green-apex.com>
2022-04-20 14:53:44 +0530

    added .env.local file for google client id

--- sb-admin/pages/api/auth/[...nextauth].js
+++ sb-admin/pages/api/auth/[...nextauth].js
@@ -9,8 +9,8 @@ export default NextAuth({
   providers: [
     // OAuth authentication providers
     GoogleProvider({
-      clientId: '629499820311-4eo0g0v90vhjibud03cr5v3p5n4njnoj.apps.googleusercontent.com',
-      clientSecret: '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',
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
